fae71caa1c4abd7790b7a8d377d0dc5cc683f887
prakashsvmx <prakash.senthilvel@ge.com>
2019-04-29 14:16:44 +0530

    SVMX_PRESET_19_2 Upgrade CodeMirror Plugin version.

--- presets/svmx-ckeditor-config.js
+++ presets/svmx-ckeditor-config.js
@@ -6,4 +6,5 @@
 CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
+	config.extraPlugins = 'textselection';
 };
